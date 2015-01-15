274fa0fc5b54422e03ccec780153537f026387c9
Evandro Macedo <evandro.hcm@gmail.com>
2015-01-15 00:28:55 -0300

    Ajeitando bugs na validação

---
+++ app/assets/javascripts/clientes.js
@@ -0,0 +1,3 @@
+$(document).ready(function() {
+	$('.field_with_errors').parent('.form-group').addClass('has-error');
+});
--- app/models/cliente.rb
+++ app/models/cliente.rb
@@ -1,4 +1,4 @@
 class Cliente < ActiveRecord::Base
-	validates :nome, format: { with: /\A[a-zA-Z]+\z/, message: "Insira apenas letras no nome" }
+	validates :nome, format: { with: /\A[a-z A-Z]+\z/, message: "Insira apenas letras e espaços no nome" }
 	validates :cpf, format: { with: /\A\d{3}.\d{3}.\d{3}-\d{2}\z/, message: "Insira o CPF no formato \"999.999.999-99\"" }
 end
