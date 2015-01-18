ac0f8202268ad02d12f6ce5e9357340e30da1872
Evandro Macedo <evandro.hcm@gmail.com>
2015-01-18 00:29:15 -0300

    Adicionando funcionalidade de gerar relatório dos acessos

--- app/controllers/acessos_controller.rb
+++ app/controllers/acessos_controller.rb
@@ -16,6 +16,72 @@ class AcessosController < ApplicationController
     	redirect_to '/acessos'
 	end
 
+	# Relatórios do HyPDF
+	def relatorio_acessos
+		html = %Q{
+			<html>
+			<head>
+				<meta charset="UTF-8">
+				<title>Relatório dos últimos Acessos</title>
+
+				<style>
+					table {
+						width: 100%;
+					}
+
+					table, tr, th, td {
+						border: 1px solid black;
+						border-collapse: collapse;
+					}
+				</style>
+			</head>
+			<body>
+				<br>
+				<h1>Relatório dos últimos Acessos da LAN</h1>
+
+				<table style="">
+					<thead>
+						<tr>
+							<th>Id</th>
+							<th>Valor</th>
+							<th>Duração</th>
+							<th>Data do Acesso</th>
+							<th>Nome do Cliente</th>
+							<th>CPF do Cliente</th>
+						</tr>
+				    </thead>
+
+				    <tbody>
+		}
+
+		Acesso.all.each do |acesso|
+			html << %Q{
+				<tr>
+		    		<td class="vert-align">#{acesso.id}</td>
+		    		<td class="vert-align">R$ #{acesso.valor}</td>
+		    		<td class="vert-align">#{acesso.duracao} h</td>
+		    		<td class="vert-align">#{acesso.created_at.to_time.strftime("%d/%m/%Y") }</td>
+		    		<td class="vert-align">#{acesso.cliente.nome}</td>
+		    		<td class="vert-align">#{acesso.cliente.cpf}</td>
+		    	</tr>
+			}
+		end
+
+		html << %Q{
+				</tbody>
+			</table>
+		</body>
+		</html>
+		}
+
+		hypdf = HyPDF.htmltopdf(
+		    html,
+		    test: true
+		)
+
+		send_data(hypdf[:pdf], filename: 'relatorio_acessos.pdf', type: 'application/pdf')
+	end
+
 	# Privates
 	private
 		def acesso_params
